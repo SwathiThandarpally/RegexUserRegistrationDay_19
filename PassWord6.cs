a13c5a923af8e8c6151034daa7380f294e1170fb
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-11-08 12:24:35 +0530

    Added usecase_6 valid user password

---
+++ PassWord6.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexPractice
+{
+    public class PassWord6
+    {
+
+        // public static string REGEX_PASSWORD = "^[A-Z]{1}[a-z]{5}$";
+        public static string REGEX_PASSWORD = "^[A-Z]{1}[a-z0-9]{5}$";  //USECASE_6&7
+
+
+        public bool validatePassWord(string password)
+        {
+            return Regex.IsMatch(password, REGEX_PASSWORD);
+        }
+    }
+}
