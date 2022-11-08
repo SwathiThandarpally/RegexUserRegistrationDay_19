c50f476f60c5fd02fbbfd7a618cc00cc1364689f
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-11-08 11:55:47 +0530

    Added user valid first name program

---
+++ ValidFirstName.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexPractice
+{
+    public class ValidFirstName
+    {
+
+        public static string REGEX_VALIDFIRSTNAME = "^[A-Z]{1}[a-z]{5}$";
+
+
+        public bool validateValidfirstName(string validfirstname)
+        {
+            return Regex.IsMatch(validfirstname, REGEX_VALIDFIRSTNAME);
+        }
+    }
+}
+
