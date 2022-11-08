45e10bfbc895c53d3a1765997bd0bfbc2702a6c8
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-11-08 11:50:48 +0530

    Added all user program

---
+++ Program.cs
@@ -0,0 +1,131 @@
+﻿namespace RegexUserRegistration
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //ValidFirstName validFirstName = new ValidFirstName1(); 
+            //Console.WriteLine("please enter the name");
+
+            //string validfirstname = Console.ReadLine();
+
+            //bool value = validFirstName.validateValidFirstName(validfirstname);
+            //if (value)
+            //{
+            //    Console.WriteLine("Name is matched");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Name is not matched");
+            //}                                                                        USECASE_1
+
+            //ValidLastName2 validLastName2 = new ValidLastName2();
+            //Console.WriteLine("please enter the name");
+
+            //string validlastname = Console.ReadLine();
+
+            //bool value = validLastName2.validateValidLastName(validlastname) ;
+            //if (value)
+            //{
+            //    Console.WriteLine("Name is matched");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Name is not matched");
+            //}                                                                        USECASE_2
+
+
+            //ValidEmail3 validEmail3 = new ValidEmail3();
+            //Console.WriteLine("please enter the EmailId");
+
+            //string validemail = Console.ReadLine();
+
+            //bool value = validEmail3.validateValidEmail( validemail );
+            //if (value)
+            //{
+            //    Console.WriteLine("Name is matched");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Name is not matched");
+            //}                                                                       USECASE_3
+
+            //MobileFormat mobileFormat = new MobileFormat();
+            //Console.WriteLine("please enter the phonenumber");
+
+            //string phonenumber = Console.ReadLine();
+
+            //bool value =mobileFormat.validatePhoneNumber(phonenumber);
+            //if (value)
+            //{
+            //    Console.WriteLine("PhoneNumber is matched");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("PhoneNumber is not matched");
+            //}                                                                       USECASE_4
+
+            //PassWord5 passWord5 = new PassWord5();
+            //Console.WriteLine("please enter the password");
+
+            //string password = Console.ReadLine();
+
+            //bool value = passWord5.validatePassWord(password);
+            //if (value)
+            //{
+            //    Console.WriteLine("password is matched");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("password is not matched");
+            //}                                                                  USECASE_5
+
+            //Password6Rule2 password6Rule2 = new Password6Rule2();
+            //Console.WriteLine("please enter the password");
+
+            //string password = Console.ReadLine();
+
+            //bool value = password6Rule2.validatePassWord(password);
+            //if (value)
+            //{
+            //    Console.WriteLine("password is matched");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("password is not matched");
+            //}                                                                USECASE_6 AND 7
+
+            //PasswordSpecialChar passwordSpecialChar = new PasswordSpecialChar();
+            //Console.WriteLine("please enter the password");
+
+            //string password = Console.ReadLine();
+
+            //bool value = passwordSpecialChar.validatePassWord(password);
+            //if (value)
+            //{
+            //    Console.WriteLine("password is matched");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("password is not matched");
+            //}                                                                  USECASE_8
+
+
+            //AllEmailId allEmailId = new AllEmailId();
+            //Console.WriteLine("please enter the EmailId");
+
+            //string validemail = Console.ReadLine();
+
+            //bool value = allEmailId.validateValidEmail(validemail);
+            //if (value)
+            //{
+            //    Console.WriteLine("Email is matched");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Email is not matched");
+            //}                                                                   USECASE_9
+
+        }
+    }
+}
