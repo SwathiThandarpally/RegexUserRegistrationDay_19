9a34e3c4453a716ffb8c240c770e7aa592c7dd2b
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-11-08 12:02:15 +0530

    Added user valid email id

---
+++ ValidEmail3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexPractice
+{
+    public class ValidEmail3
+    {
+        public static string REGEX_VALIDEMAIL = "^[abc._][-a-z0-3._]+@([co]+.)+[a-z]{2,5}$"; //^[a-z0-9][-a-z0-9._]+@([-a-z0-9]+.)+[a-z]{2,5}$
+
+
+        public bool validateValidEmail(string validemail)
+        {
+            return Regex.IsMatch(validemail, REGEX_VALIDEMAIL);
+        }
+    }
+}
