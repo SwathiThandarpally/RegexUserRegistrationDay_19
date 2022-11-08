908a795a0e19227583be0d79a0c498eb814167d3
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-11-08 11:58:36 +0530

    Added all user last name program

---
+++ ValidLastName.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexPractice
+{
+    public class ValidLastName
+    {
+        public static string REGEX_VALIDLASTNAME = "^[A-Z]{1}[a-z]{5}[' '][A-Z]{1}[a-z]{3}";
+
+
+        public bool validateValidLastName(string validlastname)
+        {
+            return Regex.IsMatch(validlastname, REGEX_VALIDLASTNAME);
+        }
+    }
+}
