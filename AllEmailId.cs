b956d3a3a496763cb9ec5636be00ceb1abf7cacf
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-11-08 12:04:46 +0530

    Added user all email id validation program

---
+++ AllEmailId.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexPractice
+{
+    public class AllEmailId
+    {
+        public static string REGEX_VALIDEMAIL = "^[a-z0-9][-a-z0-9._]+@([-a-z0-9]+.)+[a-z]{2,5}$";
+
+
+        public bool validateValidEmail(string validemail)
+        {
+            return Regex.IsMatch(validemail, REGEX_VALIDEMAIL);
+        }
+    }
+}
