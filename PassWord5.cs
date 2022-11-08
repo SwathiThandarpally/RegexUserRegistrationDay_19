d61eae1d1d8a0f03b3a2384bf95e6e59fe197ff6
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-11-08 12:22:36 +0530

    Added valid user password

---
+++ PassWord5.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexPractice
+{
+    public class PassWord5
+    {
+        public static string REGEX_PASSWORD = "^[A-Z]{1}[a-z]{7}$";
+
+        public bool validatePassWord(string password)
+        {
+            return Regex.IsMatch(password, REGEX_PASSWORD);
+        }
+    }
+}
