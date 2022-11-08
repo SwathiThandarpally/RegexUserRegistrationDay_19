02cfb359a6fdbc139b1107afb2a5c8cc5eca5229
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-11-08 12:40:59 +0530

    Added user password with special characters

---
+++ PasswordSpecialChar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexPractice
+{
+    public class PasswordSpecialChar
+    {
+        public static string REGEX_PASSWORD = "^[A-Z]{1}[a-z0-9])(?=.*\\W).{8,8})$";
+
+
+        public bool validatePassWord(string password)
+        {
+            return Regex.IsMatch(password, REGEX_PASSWORD);
+        }
+    }
+}
