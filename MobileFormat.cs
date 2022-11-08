d07caa255f08e098c34e841fc48e88ec932aad58
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-11-08 12:20:13 +0530

    Added valid user phone number program

---
+++ MobileFormat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexPractice
+{
+    public class MobileFormat
+    {
+        public static string REGEX_PHONENUMBER = "^(91)?[6-9]{1}[0-9]{9}$";
+
+        public bool validatePhoneNumber(string phonenumber)
+
+        {
+            return Regex.IsMatch(phonenumber, REGEX_PHONENUMBER);
+        }
+    }
+}
